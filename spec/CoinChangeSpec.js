4a76e15b94f8297861e9864d74f8f7a80436d8af
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-02-15 17:47:10 +0100

    added changeCurrency and added gulden currency

--- spec/CoinChangeSpec.js
+++ spec/CoinChangeSpec.js
@@ -1,19 +1,19 @@
 describe ("coin changer", function (){
     it ("should return 1 * 100 cent of input = 100", function(){
         var changer1 = new CoinChanger ();
-        expect (changer1.showCoins(100)).toEqual ([1,0,0,0,0,0,0]);
+        expect (changer1.showCoins(100)).toEqual ([0,1,0,0,0,0,0,0]);
     });
     it ("should return 2 * 100 and 1 * 50 cent of input = 250", function(){
         var changer1 = new CoinChanger ();
-        expect (changer1.showCoins(250)).toEqual ([2,1,0,0,0,0,0]);
+        expect (changer1.showCoins(250)).toEqual ([1,0,1,0,0,0,0,0]);
     });
     it ("should return 1 * 50 cent of input = 50", function(){
         var changer1 = new CoinChanger ();
-        expect (changer1.showCoins(50)).toEqual ([0,1,0,0,0,0,0]);
+        expect (changer1.showCoins(50)).toEqual ([0,0,1,0,0,0,0,0]);
     });
     it ("should return 1 * 20 cent of input = 20", function(){
         var changer1 = new CoinChanger ();
-        expect (changer1.showCoins(20)).toEqual ([0,0,1,0,0,0,0]);
+        expect (changer1.showCoins(20)).toEqual ([0,0,0,1,0,0,0,0]);
     });
     it ("should tell: The coins you require: 1 times 20 cents of input = 20", function(){
         var changer1 = new CoinChanger ();
@@ -28,5 +28,10 @@ describe ("coin changer", function (){
         expect (changer1.declareCoins(77)).toEqual ("The coins you require: 1 times 50 cents, 1 times 20 cents, 1 times 5 cents, 1 times 2 cents.");
     });
 
+    it("should change the coinsystem to 'guldens'", function(){
+       var changer1 = new CoinChanger();
+       changer1.changeCurrency("gulden");
+       expect(changer1.coins).toEqual([500,250,100,50,25,10,5])
+    });
 
 });
--- src/CoinChanger.js
+++ src/CoinChanger.js
@@ -19,6 +19,15 @@ function CoinChanger(){
         }
         return "The coins you require:" + declareArray.toString() + ".";
     }
+
+    this.changeCurrency =  function(currency){
+        if(currency === "gulden"){
+            return this.coins = [500,250,100,50,25,10,5];
+        }
+        else if(currency === "Euro"){
+            return this.coins = [200,100,50,20,10,5,2,1]
+        }
+    };
 };
 
 CoinChanger.prototype.showCoins = function (amount){
@@ -30,4 +39,4 @@ CoinChanger.prototype.declareCoins = function (amount){
     return this.declareNumberOfCoins();
 };
 
-CoinChanger.prototype.coins = [100 , 50 , 20 , 10 , 5 , 2 , 1];
+CoinChanger.prototype.coins = [200,100,50,20,10,5,2,1];

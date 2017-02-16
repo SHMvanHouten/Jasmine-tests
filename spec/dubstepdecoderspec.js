15647d43e968c2c0cc1f3fb010f4074ce0511ae2
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-02-16 11:08:27 +0100

    So this is the best way to do it, using regexp and .trim()...

--- spec/dubstepdecoderspec.js
+++ spec/dubstepdecoderspec.js
@@ -1,28 +1,28 @@
 describe("Dubstep Decoder", function(){
 
-    xit("should remove the first WUB and second WUB and return A B C", function(){
+    it("should remove the first WUB and second WUB and return A B C", function(){
         var decoder1 = new DubstepDecoder();
-        expect(decoder1.showDecodedString("AWUBBWUBC")).toEqual("A B C")
+        expect(decoder1.getDecodedString("AWUBBWUBC")).toEqual("A B C")
     });
-    xit("should remove the firs and second WUB and return A B C", function(){
+    it("should remove the firs and second WUB and return A B C", function(){
         var decoder1 = new DubstepDecoder();
-        expect(decoder1.showDecodedString("1WUB2WUB3")).toEqual("1 2 3")
+        expect(decoder1.getDecodedString("1WUB2WUB3")).toEqual("1 2 3")
     });
-    xit("should remove the first WUB and second WUB and third WUB and return A B C", function(){
+    it("should remove the first WUB and second WUB and third WUB and return A B C", function(){
         var decoder1 = new DubstepDecoder();
-        expect(decoder1.showDecodedString("1WUB2WUBWUB3")).toEqual("1 2 3")
-    });
-    xit("should remove all the WUBS and return A B C", function(){
-        var decoder1 = new DubstepDecoder();
-        expect(decoder1.showDecodedString("AWUBWUBWUBBWUBWUBWUBC")).toEqual("A B C")
-    });
-    xit("should remove all the WUBS and return A B C", function(){
-        var decoder1 = new DubstepDecoder();
-        expect(decoder1.showDecodedString("WUBAWUBBWUBCWUB")).toEqual("A B C")
+        expect(decoder1.getDecodedString("1WUB2WUBWUB3")).toEqual("1 2 3")
     });
     it("should remove all the WUBS and return A B C", function(){
         var decoder1 = new DubstepDecoder();
-        expect(decoder1.showDecodedString("WUBAWUBBWUBCWUBWUBWUB")).toEqual("A B C")
+        expect(decoder1.getDecodedString("AWUBWUBWUBBWUBWUBWUBC")).toEqual("A B C")
+    });
+    it("should remove all the WUBS and return A B C", function(){
+        var decoder1 = new DubstepDecoder();
+        expect(decoder1.getDecodedString("WUBAWUBBWUBCWUB")).toEqual("A B C")
+    });
+    it("should remove all the WUBS and return A B C", function(){
+        var decoder1 = new DubstepDecoder();
+        expect(decoder1.getDecodedString("WUBAWUBBWUBCWUBWUBWUB")).toEqual("A B C")
     });
 
 
--- src/dubstepdecoder.js
+++ src/dubstepdecoder.js
@@ -1,53 +1,5 @@
 function DubstepDecoder(){
-    var song;
-    var wub = "WUB";
-    var wubSlam
-    var wubCounter = 0;
-    var replaceForSpace = function replaceForSpace(){
-        return song = song.replace(wub , " ")
-    };
-    var replaceNoSpace =  function replaceNoSpace(){
-        return song = song.replace(wub , "")
-    }
-//    var howManyWubs = function(){
-//        for (wubCounter = 0; wubCounter < (198/3); wubCounter++){
-//            if (wubCounter === song.indexOf(wubSlam)){
-//                wubSlam += wub;
-//            }
-//        }
-//    }
-//
-    var replaceTheWub = function replaceTheWub(){
-
-        // if ((WUB is on position 0 || WUB has a space in front ) {replace with empty}
-        if ((song.indexOf(wub) === 0) || (song.indexOf(" " + wub) !== -1)){
-            song = replaceNoSpace(song);
-        }
-        // else if (last WUB is at the end) && (last WUB is only WUB)
-        else if ((song.lastIndexOf(wub) === (song.length - 3)) && (song.lastIndexOf(wub))===(song.indexOf(wub))){
-            song = replaceNoSpace(song);
-        }
-        //else if (there is a WUBWUB) && (those are the last WUBs)
-        else if (song.lastIndexOf(wub+wub) === (song.length - 6) && (song.lastIndexOf(wub) - 3)===(song.indexOf(wub))){
-            song = replaceNoSpace();
-        }
-        else{
-            song = replaceForSpace();
-        };
-        return song
-    };
-
     this.getDecodedString = function getDecodedString(input){
-        song = input
-        while(song.indexOf(wub) !== -1){
-            song = replaceTheWub();
-        };
-        while(song.lastIndexOf(" ") === song.length - 1){
-            song = song.slice(0, -1)
-        }
-        return song;
-    };
+        return input.replace(/(WUB)+/g," ").trim();
+    }
 };
-DubstepDecoder.prototype.showDecodedString = function showDecodedString(input){
-        return this.getDecodedString(input);
-    };

b92e407e45f3f4f45e2e7e6d209de75fbd4acb77
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-02-21 13:57:31 +0100

    added 13 to each of the char codes in the array to encode it.

--- spec/rot13encoderspec.js
+++ spec/rot13encoderspec.js
@@ -1,11 +1,18 @@
 describe("Rot13Encoder", function(){
-    it("should return an array of character codes", function(){
+    xit("should return an array of character codes", function(){
         encoder = new Rot13Encoder("test");
+        encoder.getNewCodesArray();
         expect(encoder.showCodesArray()).toEqual([116,101,115,116]);
     })
-    it("should return an array of character codes", function(){
+    xit("should return an array of character codes", function(){
         encoder = new Rot13Encoder("testtt");
+        encoder.getNewCodesArray();
         expect(encoder.showCodesArray()).toEqual([116,101,115,116,116,116]);
+    })
+    it("should return an array of +13 character codes for each number in the codesArray", function(){
+        encoder = new Rot13Encoder("test");
+        encoder.getNewCodesArray();
+        expect(encoder.showCodesArray()).toEqual([129,114,128,129]);
     })
 
 });
--- src/rot13encoder.js
+++ src/rot13encoder.js
@@ -1,16 +1,17 @@
 function Rot13Encoder(message){
-    var codesArray = [];
-    this.getCodesArray = function(){
+    var newCodesArray = []
+    this.getNewCodesArray = function(){
 //        var a = map.call('Hello World', function(x) {
 //          return x.charCodeAt(0);
 //        });
         var map = Array.prototype.map;
-        codesArray = map.call(message, function(x){
-            return x.charCodeAt(0);
+        newCodesArray = map.call(message, function(x){
+            var code = x.charCodeAt(0);
+            return (code + 13);
         });
     };
     this.showCodesArray = function(){
-        this.getCodesArray();
-        return codesArray;
+
+        return newCodesArray;
     }
 };

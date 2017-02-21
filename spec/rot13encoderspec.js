1585b8d4e886e4ae44227ea1850ef10c81dd13a4
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-02-21 13:48:26 +0100

    added a map function (to the string with a method found on mdn) to return the character codes of each character in the string to a new array.

--- spec/rot13encoderspec.js
+++ spec/rot13encoderspec.js
@@ -3,4 +3,9 @@ describe("Rot13Encoder", function(){
         encoder = new Rot13Encoder("test");
         expect(encoder.showCodesArray()).toEqual([116,101,115,116]);
     })
+    it("should return an array of character codes", function(){
+        encoder = new Rot13Encoder("testtt");
+        expect(encoder.showCodesArray()).toEqual([116,101,115,116,116,116]);
+    })
+
 });
--- src/rot13encoder.js
+++ src/rot13encoder.js
@@ -1,6 +1,16 @@
 function Rot13Encoder(message){
-    codesArray = [116,101,115,116]
+    var codesArray = [];
+    this.getCodesArray = function(){
+//        var a = map.call('Hello World', function(x) {
+//          return x.charCodeAt(0);
+//        });
+        var map = Array.prototype.map;
+        codesArray = map.call(message, function(x){
+            return x.charCodeAt(0);
+        });
+    };
     this.showCodesArray = function(){
+        this.getCodesArray();
         return codesArray;
     }
 };

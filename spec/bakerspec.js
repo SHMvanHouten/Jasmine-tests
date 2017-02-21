e02c18ccb058d97a4fb879e4b9cbb11259d4b5e9
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-02-21 18:30:59 +0100

    got a basic checker to compare ingredients to stock items.

--- spec/bakerspec.js
+++ spec/bakerspec.js
@@ -23,5 +23,10 @@ describe("BakerHelper", function(){
         helper = new BakerHelper({apples: 3, flour: 500, sugar: 200, eggs: 1},{flour: 1200, sugar: 1200, eggs: 5, milk: 200});
         expect(helper.getStock()).toEqual(["flour", "sugar", "eggs", "milk"]);
     });
+    it("should check if each item in the ingredients list is accounted for in the stock list", function(){
+        helper = new BakerHelper({apples: 3, flour: 500, sugar: 200, eggs: 1},{flour: 1200, sugar: 1200, eggs: 5, milk: 200});
+        expect(helper.checkStockSufficiency()).toEqual(false);
+    });
+
 
 })
--- src/baker.js
+++ src/baker.js
@@ -5,4 +5,17 @@ function BakerHelper(recipe, stock){
     this.getStock = function(){
         return Object.getOwnPropertyNames(stock);
     };
+    this.checkStockSufficiency =  function(){
+        var ingredients = this.getRecipeIngredients();
+        var stock = this.getStock();
+        var itemCheck;
+        for(var i = 0; i < ingredients.length; i++){
+            itemCheck = false;
+            for(var j = 0; j < stock.length; j++){
+                if(stock[j]=== ingredients[i]){itemCheck = true;}
+            };
+            if(itemCheck === false){return false}
+        };
+        return true
+    };
 }

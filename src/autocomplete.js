9238b436c2684e054101a59c1e6a18aaa2510629
20130311RLE1\Eigenaar <svanhouten@gmail.com>
2017-02-22 14:59:00 +0100

    cleaned up code slightly

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,11 +1,10 @@
 function Autocomplete(input, dictionary){
     var matches = [];
-    var input = input.replace(/[^A-Za-z]/g, "");
+    var input = input.replace(/[^A-Za-z]/g, "").toLowerCase();
 
     this.getMatches = function(){
         dictionary.forEach(function(element){
-            elementReplacement = element.replace(/[^A-Za-z]/g, "");
-            elementReplacement = elementReplacement.toLowerCase();
+            elementReplacement = element.replace(/[^A-Za-z]/g, "").toLowerCase();
             if (elementReplacement.indexOf(input)===0 && matches.length < 5){matches.push(element)}
         });
         return matches;

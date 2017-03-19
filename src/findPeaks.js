34d48df6455759e9659bc661c89d52e111cb8ff7
Sjoerd <svanhouten@gmail.com>
2017-03-19 16:48:20 +0100

    made the findPeaks function where the objective will be to locate the peaks in a landscape and to return their locations and heights.

---
+++ src/findPeaks.js
@@ -0,0 +1,8 @@
+function PeakFinder(){
+
+}
+PeakFinder.prototype.getAllPeakLocationsAndHeights = function(landScape){
+    var result = {pos:[],peaks:[2]};
+    result.pos.push(landScape.indexOf(2));
+    return result;
+};

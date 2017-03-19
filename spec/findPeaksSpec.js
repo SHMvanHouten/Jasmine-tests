f97eae975791cd1f6c7d6cae87befe72ecaaea74
Sjoerd <svanhouten@gmail.com>
2017-03-19 17:29:05 +0100

    now locates plateaus but fails if the plateau is immediatly followed by a higher peak.

--- spec/findPeaksSpec.js
+++ spec/findPeaksSpec.js
@@ -39,6 +39,17 @@ describe("PeakFinder",function(){
         var landScape = [1,1,3,1,1,5,2];
         expect(finder.getAllPeakLocationsAndHeights(landScape)).toEqual({pos:[2,5],peaks:[3,5]});
     });
+    it("should return an object defining the peak to be at index 2 and for it to be 3 high ",function(){
+        finder = new PeakFinder();
+        var landScape = [1,2,3,1,1,1];
+        expect(finder.getAllPeakLocationsAndHeights(landScape)).toEqual({pos:[2],peaks:[3]});
+    });
+    xit("should return an object defining the peak to be at index 2 and for it to be 3 high ",function(){
+        finder = new PeakFinder();
+        var landScape = [1,2,3,3,3,4,1];
+        expect(finder.getAllPeakLocationsAndHeights(landScape)).toEqual({pos:[2],peaks:[3]});
+    });
+
 
 
 

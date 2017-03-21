ce5a976136357c4df50a80ae1a6d4e7c6bb8ddde
Sjoerd <svanhouten@gmail.com>
2017-03-21 17:40:12 +0100

    added a function that does it all directly to paste into codewars.

--- src/justify.js
+++ src/justify.js
@@ -1,3 +1,5 @@
+
+
 function Justify(){
     var getStringOfSpaces = function(n){
         var string = "";
@@ -31,7 +33,7 @@ function Justify(){
         var wordsArray = text.split(" ");
         var fittingSentencesArray = [[wordsArray[0]]];
         var lineLength = wordsArray[0].length;
-        var i = 1, j = 0, k=0;
+        var i = 1, j = 0;
         for(i; i<wordsArray.length; i += 1){
             lineLength += wordsArray[i].length +1;
             // the +1 accounts for the space
@@ -57,10 +59,74 @@ Justify.prototype.getJustifiedText = function(text, length){
     var lengthDifference;
     for(var i = 0; i< fittingSentencesArrayLength-1; i += 1){
         lengthDifference = this.getLengthDifference(fittingSentencesArray[i], length);
-        if (lengthDifference === 0){justifiedText += fittingSentence.join("")}
+        if (lengthDifference === 0){justifiedText += fittingSentencesArray[i].join("")}
         else{justifiedText += this.addSpaces(fittingSentencesArray[i], lengthDifference)}
         justifiedText += "\n";
     };
     justifiedText += fittingSentencesArray[fittingSentencesArrayLength-1].join("");
     return justifiedText;
+};
+
+
+var justify = function(str, len) {
+    var getStringOfSpaces = function(n){
+        var string = "";
+        for(var i = 0; i<n; i+= 1){
+            string += " ";
+        }
+        return string;
+    };
+
+
+    this.addSpaces = function(lineArray,lineLengthDifference) {
+        var numberOfWordsInArray = lineArray.length;
+        var extraSpacesAtBeginningOfLine = 0;
+        while(lineLengthDifference % (numberOfWordsInArray -1) >0){
+            extraSpacesAtBeginningOfLine +=1;
+            lineLengthDifference -= 1;
+        }
+        var amountOfSpacesPerWord = lineLengthDifference/ (numberOfWordsInArray -1);
+        var stringOfSpacesToInsert = getStringOfSpaces(amountOfSpacesPerWord);
+        var line = lineArray[0];
+        for(var i=1; i<numberOfWordsInArray; i += 1){
+            if(extraSpacesAtBeginningOfLine > 0){
+                line += " ";
+                extraSpacesAtBeginningOfLine -= 1;
+            }
+            line += stringOfSpacesToInsert + lineArray[i];
+        }
+        return [line];
+    };
+    this.getJustifiedText = function(text, length){
+        var wordsArray = text.split(" ");
+        var fittingSentencesArray = [wordsArray[0]];
+        var lineLength = wordsArray[0].length;
+        var i = 1;
+        var justifiedText = "";
+        for(i; i<wordsArray.length; i += 1){
+            lineLength += wordsArray[i].length +1;
+            // the +1 accounts for the space
+            if(lineLength <= length){
+                fittingSentencesArray.push(" " + wordsArray[i]);
+                if(i === wordsArray.length -1){
+                    justifiedText += fittingSentencesArray.join("")
+                }
+            }else{
+                var fittingSentencesArrayJoined = fittingSentencesArray.join("");
+                var lengthDifference = length - fittingSentencesArrayJoined.length;
+                if(lengthDifference === 0){
+                    justifiedText += fittingSentencesArrayJoined;
+                }else{
+                justifiedText += this.addSpaces(fittingSentencesArray, lengthDifference);
+                }
+                justifiedText += "\n";
+
+                lineLength = wordsArray[i].length;
+                if(lineLength >= length){justifiedText += wordsArray[i]}
+                else{fittingSentencesArray = [wordsArray[i]];}
+            }
+        }
+        return justifiedText;
+    };
+    return this.getJustifiedText(str, len);
 };
